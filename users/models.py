0b01ed189ccd306839c90a4947a398a87ed353fa
Nitipong Kadsritalee <nitipongkadsritalee01@gmail.com>
2024-11-10 12:28:29 +0700

    WIP models for users

--- users/models.py
+++ users/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Account(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # the schedule table of another person
+    # REQUIRE schedule models
+
+    # time_table = models.............
+    # schedule = models...................
+
+class Group(models.Model):
+    members = models.ManyToManyField(Account, blank=True, related_name="groupings")
+
+    def __str__(self):
+        return f"{self.members}"
