58259a001723d87f59ec0a7a3c43304ecda29211
Nitipong Kadsritalee <nitipongkadsritalee01@gmail.com>
2024-11-16 07:50:01 +0700

    Calendar v0.2

--- event/models.py
+++ event/models.py
@@ -7,7 +7,7 @@ class Event(models.Model):
     date = models.DateField("Day of Event")
     start_time = models.TimeField("Start Time")
     end_time = models.TimeField("End Time")
-    text = models.TextField("Text", blank=True, null=True)
+    text = models.TextField("Text", blank=True, null=True, max_length=20)
 
     def __str__(self):
         return f"{self.start_time}"
@@ -15,3 +15,9 @@ class Event(models.Model):
     def clean(self):
         if self.end_time <= self.start_time:
             raise ValidationError("End Time can't be before Start Time.")
+        
+    def getMonth(self):
+        return f"{self.date.year}"
+    
+    def getYear(self):
+        return f"{self.date.month}"
--- event/views.py
+++ event/views.py
@@ -32,12 +32,22 @@ def calendar_view(request, year = None, month = None):
         prev_year = year - 1
     else:
         prev_month = month - 1
-        prev_year = year - 1
+        prev_year = year
 
     cal = calendar.Calendar(6)                          # 6, So sunday is the first. Maybe changed later.
     days_in_month = cal.monthdayscalendar(year, month)
 
-    
+    sorted_events = Event.objects.all().order_by("start_time")              # Sort event by time first
+    all_events = sorted_events.filter(date__year=year, date__month=month)   # Get events connected to this year and month
+
+    events_per_day = {}
+    for event in all_events:
+        day = event.date.day                    # get day of event date
+        if day not in events_per_day:           # if this day isn't already in the list, 
+            events_per_day[day] = []            # create list for that day
+        events_per_day[day].append(event)       # add event into that day
+
+    print(events_per_day)
 
     # Force Sunday to start first
     # Wouldn't have to do this if setfirstweek() actually work
@@ -53,4 +63,5 @@ def calendar_view(request, year = None, month = None):
         'month_name': calendar.month_name[month],
         'month_days': days_in_month,
         'day_names': day_names, 
+        'events': events_per_day,
     })
