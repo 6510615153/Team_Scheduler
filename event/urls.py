f432b088a09df6598f1b41fc8b421cf7e7b5686b
Nitipong Kadsritalee <nitipongkadsritalee01@gmail.com>
2024-11-16 21:14:05 +0700

    Group can now see member's event v0.1

--- event/urls.py
+++ event/urls.py
@@ -7,5 +7,9 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('calendar/', views.calendar_view, name='calendar'),  
     path('calendar/<int:year>/<int:month>/', views.calendar_view, name='calendar_change'),
+
     path('calendar/eventadd', views.event_add, name='event_add'),
+
+    path('<str:code>/calendar/', views.calendar_view_group, name='calendar_group'),  
+    path('<str:code>/calendar/<int:year>/<int:month>/', views.calendar_view_group, name='calendar_change_group'),
 ]
